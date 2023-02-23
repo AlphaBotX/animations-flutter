78bf36e12b8254f5407027b0fe8e9f6a3e1a93aa
BabishShrestha <babishshrestha8@gmail.com>
2023-02-23 17:00:31 +0545

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math' show pi;
+
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,109 +21,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({
     super.key,
   });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation _animation;
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-    _animation = Tween<double>(
-      begin: 0,
-      end: 2 * pi,
-    ).animate(_animationController);
-    _animationController.repeat();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animationController,
-          builder: (context, child) => Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()..rotateY(_animation.value),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  decoration: const BoxDecoration(
-                    borderRadius:
-                        BorderRadius.horizontal(left: Radius.circular(200)),
-                    color: Colors.green,
-                    // boxShadow: [
-                    //   BoxShadow(
-                    //     color: Colors.black.withOpacity(0.5),
-                    //     blurRadius: 7,
-                    //     spreadRadius: 5,
-                    //     offset: const Offset(0, 3),
-                    //   ),
-                    // ],
-                  ),
-                ),
-
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  decoration: const BoxDecoration(
-                    borderRadius:
-                        BorderRadius.horizontal(right: Radius.circular(200)),
-                    color: Colors.yellow,
-                    // boxShadow: [
-                    //   BoxShadow(
-                    //     color: Colors.black.withOpacity(0.5),
-                    //     blurRadius: 7,
-                    //     spreadRadius: 5,
-                    //     offset: const Offset(0, 3),
-                    //   ),
-                    // ],
-                  ),
-                ),
-
-                ///Animation for Y axis flip
-                // Container(
-                //   height: 150,
-                //   width: 150,
-                //   decoration: BoxDecoration(
-                //     borderRadius: BorderRadius.circular(100),
-                //     color: Colors.green,
-                //     boxShadow: [
-                //       BoxShadow(
-                //         color: Colors.black.withOpacity(0.5),
-                //         blurRadius: 7,
-                //         spreadRadius: 5,
-                //         offset: const Offset(0, 3),
-                //       ),
-                //     ],
-                //   ),
-                // ),
-              ],
-            ),
-          ),
-        ),
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Home(),
       ),
     );
   }
